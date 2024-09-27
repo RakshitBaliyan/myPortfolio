f738408abbd24304ca8a4c20e60bea3887f19521
unknown <rakshitbaliyan6410@gmail.com>
2024-09-27 18:04:03 +0530

    footer file changed

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
 import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
