dc5d3ae4a75fd04dde3dabae3a03825fe3f1c932
unknown <rakshitbaliyan6410@gmail.com>
2024-09-27 18:06:22 +0530

    again home modified

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,7 +3,7 @@ import './hero.modular.css';
 import gif from '../../assets/hero-gif.gif';
 import arrowRight from '../../assets/arrow-right.svg';
 import arrowDown from '../../assets/arrow-down.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
 import { faLinkedin, faGithub, faInstagram } from '@fontawesome/free-brands-svg-icons';
 import { Link } from 'react-scroll';
 
